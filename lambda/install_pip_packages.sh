5ae184bf8d45b1a986b8e5f6a1d8b791b3b89264
Ryota Arai <ryota.arai@gmail.com>
2016-11-12 22:32:43 +0900

    Rename install_packages.sh

--- lambda/install_pip_packages.sh
+++ lambda/install_pip_packages.sh
@@ -1,6 +1,8 @@
 docker run -it 137112412989.dkr.ecr.us-west-2.amazonaws.com/amazonlinux:latest -v $(pwd):/work /bin/bash -c '
 yum install -y python27-pip python27-devel gcc libxml2-devel libxslt-devel &&
-cd /work/functions/crawl &&
 pip install --upgrade pip &&
+cd /work/functions/crawl &&
+/usr/local/bin/pip install -r requirements.txt -t . &&
+cd /work/functions/worker &&
 /usr/local/bin/pip install -r requirements.txt -t .
 '
